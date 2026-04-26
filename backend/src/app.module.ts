73b780a371b2d93b1a0c1eef03b418d6b77ae40a
Hariharan19A <hariharanayyappan019@gamil.com>
2026-04-26 15:19:30 +0530

    stashed changes

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
 import { UserController } from './user/user.controller';
+import { ProductsModule } from './modules/products/products.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, ProductsModule],
   controllers: [AppController, UserController],
   providers: [AppService],
 })
--- backend/src/modules/user/entities/user.entity.ts
+++ backend/src/modules/user/entities/user.entity.ts
@@ -3,4 +3,7 @@ import { Entity } from "typeorm";
 @Entity('user')
 export class User{
     userId!:number;
+    userName!:string;
+    emailId!:string;
+    password!:string;
 }
