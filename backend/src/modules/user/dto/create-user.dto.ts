ffd7c264fb4e4ca179cf5e29fb00163abfa5504f
Hariharan19A <hariharanayyappan019@gamil.com>
2026-04-26 18:47:02 +0530

    regex added

--- backend/src/modules/user/dto/create-user.dto.ts
+++ backend/src/modules/user/dto/create-user.dto.ts
@@ -1,6 +1,12 @@
+import { IsEmail, IsString, Matches } from "class-validator";
+
 export class CreateUserDTO{
-    userId!:number;
+    @IsString()
     userName!:string;
+    @IsEmail()
     emailId!:string;
+    @Matches(/^(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*_-])(?=.*[0-9]).{8,20}$/,{
+        message:"Password must contain atleast 8 characters, one uppercase, one lowercase, one number and one special character"
+    })
     password!:string;
 }
